730d25290e089562d9d07eca1b0b460b16934a89
Adrian Gabriel <90657835+AdrianGKS@users.noreply.github.com>
2023-04-03 07:46:09 -0300

    vendedor classe entidade

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,13 +1,17 @@
 package application;
 
 import model.entities.Departamento;
+import model.entities.Vendedor;
+
+import java.util.Date;
 
 public class Application {
 
     public static void main(String[] args) {
 
         Departamento obj = new Departamento(1,"Livros");
-
+        Vendedor vendedor = new Vendedor(21, "Lauro", "lauro@gmail.com", new Date(), 3000.00, obj);
         System.out.println(obj);
+        System.out.println(vendedor);
     }
 }
